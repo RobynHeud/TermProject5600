d66fd66a619a74c20d646514383dc287f8c66931
Jess Campbell <robynHeud@gmail.com>
2020-04-04 23:36:37 -0600

    Log updates, fixed sat_position bug

--- satellite.py
+++ satellite.py
@@ -1,12 +1,8 @@
 import sys
-import helper_functions as helper
 import numpy as np
-import scipy.constants
-import logging
-import os
-from decimal import *
 import helper_functions as helper
 import math
+
 
 # Make a list of satellites, with their initial starting positions
 class Satellite:
@@ -14,7 +10,7 @@ class Satellite:
     A representation for a Satellite with a label (id number) sending a signal (x, y, z) at time t
     """
 
-    def __init__(self, label, u_1, u_2, u_3, v_1, v_2, v_3, theta):
+    def __init__(self, label, u_1=0, u_2=0, u_3=0, v_1=0, v_2=0, v_3=0, theta=0):
         self.label = int(label)
         self.u_1 = u_1
         self.u_2 = u_2
@@ -34,81 +30,97 @@ class Satellite:
         v = np.array([self.v_1, self.v_2, self.v_3])
         return height * (math.cos(inner_value) * u + math.sin(inner_value) * v)
 
-# Data from data.dat file
-u = [[0 for x in range(3)] for y in range(24)]
-v = [[0 for x in range(3)] for y in range(24)]
-period = [0 for x in range(24)]
-altitude = [0 for x in range(24)]
-phase = [0 for x in range(24)]
+
+# List of satellites in orbit
 sat_list = list()
+
+# Get satellite details from data.dat file and populate the satellite list
+with open("all/data.dat", "r") as data:
+    sat_num = 0
+    new_sat = Satellite(sat_num)
+
+    for idx, line in enumerate(data):
+        index = idx % 7
+        line_info = line.strip().split("/=")
+
+        if index == 0:
+            new_sat.u_1 = float(line_info[0])
+        elif index == 1:
+            new_sat.u_2 = float(line_info[0])
+        elif index == 2:
+            new_sat.u_3 = float(line_info[0])
+        elif index == 3:
+            new_sat.v_1 = float(line_info[0])
+        elif index == 4:
+            new_sat.v_2 = float(line_info[0])
+        elif index == 5:
+            new_sat.v_3 = float(line_info[0])
+        elif index == 6:
+            new_sat.theta = float(line_info[0])
+            sat_list.append(new_sat)
+            sat_num += 1
+            new_sat = Satellite(sat_num)
 
 # Creates and writes to log file
 with open("Satellite.log", "w") as log:
     log.write("Satellite Log, Jess Campbell, Austin Watkins, Carlos Guerra\n")
-    log.write("\n  data.dat: \n\n")
+    log.write("\nSimulation Details:\n")
 
-    # Writes/Reads information from data.dat
-    with open("all/data.dat", "r") as data:
-        SatNumb = 0
-        index = 0
-        for line in data:
-            line_info = line.split("/=")
+    log.write("pi = " + str(math.pi))
+    log.write("\nspeed of light = " + str(helper.c))
+    log.write("\nradius of earth = " + str(helper.R))
+    log.write("\nsidereal day = " + str(helper.s))
+    log.write("\nsatellite period = " + str(helper.p))
+    log.write("\nsatellite altitude = " + str(helper.h))
 
-            tempIndex = (index - 4) % 9
+    # Log the satellite info
+    for sat in sat_list:
+        log.write("\n\nSatellite " + str(sat.label) + ":")
+        log.write("\nu = [" + str(sat.u_1) + ", " + str(sat.u_2) + ", " + str(sat.u_3) + "]")
+        log.write("\nv = [" + str(sat.v_1) + ", " + str(sat.v_2) + ", " + str(sat.v_3) + "]")
+        log.write("\ntheta = " + str(sat.theta))
 
-            if tempIndex == 0 or tempIndex == 1 or tempIndex == 2:
-                u[SatNumb][tempIndex] = float(line_info[0])
-                log.write("u[{Sat}][{Index}] = {Info}\n".format(Sat=SatNumb, Index=tempIndex, Info=line_info[0]))
-                index = index + 1
-            elif tempIndex == 3 or tempIndex == 4 or tempIndex == 5:
-                v[SatNumb][tempIndex % 3] = line_info[0]
-                log.write(
-                    "v[{Sat}][{Index}] = {Info}\n".format(Sat=SatNumb, Index=(tempIndex % 3), Info=line_info[0]))
-                index = index + 1
-            elif tempIndex == 8:
-                phase[SatNumb] = line_info[0]
-                log.write("phase[{Sat}] = {Info}\n".format(Sat=SatNumb, Info=line_info[0]))
-                index = index + 1
-                SatNumb = SatNumb + 1
-            else:
-                continue
-        epoch = 0
-        # Take in line with timestamp, latitude (3 parts), NS, longitude (3 parts), EW, and height
-        for line in sys.stdin:
-            log.write(" --- epoch = {epoch_numb}\n".format(epoch_numb = epoch))
-            log.write("read {info}\n".format(info = line))
-            # Parse the string and convert to Float64
-            line_array = line.split()
-            value_array = []
-            for elm in line_array:
-                new_elm = float(elm)
-                value_array.append(new_elm)
+    # Take in line with timestamp, latitude (3 parts), NS, longitude (3 parts), EW, and height
+    for epoch, line in enumerate(sys.stdin):
+        log.write("\nEpoch = {epoch_numb}".format(epoch_numb=epoch))
+        log.write("\nInput: {info}".format(info=line))
 
-            # Translate to Cartesian coordinates with rotation
-            cart_coords = helper.polar_to_cart(*value_array)
-            print(cart_coords)
+        # Parse the string and convert to Float64
+        line_array = line.split()
+        value_array = []
+        for elm in line_array:
+            new_elm = float(elm)
+            value_array.append(new_elm)
 
-            t_v = value_array[0]
-            # Use new coordinates to determine what satellites are in view/above the horizon
-            sat_in_view = []
-            for sat in sat_list:
-                sat_position = np.array(sat.get_curr_position(t_v))
-                if helper.above_horizon(*cart_coords, sat_position[0][0], sat_position[0][1], sat_position[0][2]):
-                    sat_in_view.append(sat)
+        # Translate to Cartesian coordinates with rotation
+        cart_coords = helper.polar_to_cart(*value_array)
 
-            log.write("Wrote:\n")
-            for sat in sat_in_view:
-                done = False
-                current = t_v
-                while not done:                    
-                    next = t_v - np.linalg.norm(sat.get_curr_position(current)-cart_coords)/helper.c
-                    if abs(next-current) < Decimal(0.01/helper.c):
-                        done = True
-                    current = next
+        # vehicle timestamp
+        t_v = value_array[0]
 
-                sat_position = np.array(sat.get_curr_position(current))
-                log.write("{Sat_Num} {Time_Curr} {Sat_Pos_1} {Sat_Pos_2} {Sat_Pos_3}".format(Sat_Num = sat, Time_Curr = current, Sat_Pos_1 = sat_position[0][0], Sat_Pos_2 = sat_position[0][1], Sat_Pos_3 = sat_position[0][2]))
-                # print(sat, current, sat_position[0][0], sat_position[0][1], sat_position[0][2])
-            epoch += 1
-            print("yep")
-            # Output index, timestamp, and cartesian coordinate location to stdout
+        # Use new coordinates to determine what satellites are in view/above the horizon
+        sat_in_view = []
+        for sat in sat_list:
+            sat_position = np.array(sat.get_curr_position(t_v))
+            if helper.above_horizon(*cart_coords, sat_position[0], sat_position[1], sat_position[2]):
+                sat_in_view.append(sat)
+
+        # Find the timestamp for each satellite
+        log.write("\nOutput:")
+        for sat in sat_in_view:
+            # set current to the vehicle timestamp to start
+            last_ts = t_v
+            done = False
+            while not done:
+                current_ts = t_v - np.linalg.norm(sat.get_curr_position(last_ts) - cart_coords) / helper.c
+                if abs(current_ts - last_ts) < 0.01 / helper.c:
+                    done = True
+                last_ts = current_ts
+
+            sat_position = np.array(sat.get_curr_position(current_ts))
+            log.write("\n{Sat_Num} {Time_Curr} {Sat_Pos_1} {Sat_Pos_2} {Sat_Pos_3}".format(Sat_Num=sat.label,
+                                                                                           Time_Curr=current_ts,
+                                                                                           Sat_Pos_1=sat_position[0],
+                                                                                           Sat_Pos_2=sat_position[1],
+                                                                                           Sat_Pos_3=sat_position[2]))
+            print(sat.label, current_ts, sat_position[0], sat_position[1], sat_position[2])
